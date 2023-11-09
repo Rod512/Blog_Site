59af653c72e1dec03817871f98c4a3927d8d9edb
Rudro Dhar <rudro149187@bsdi-bd.org>
2023-11-10 03:23:56 +0600

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
+    const navItems = [
+        {path: "/", link:"Home"},
+        {path: "/Services", link:"Services"},
+        {path: "/About", link:"About"},
+        {path: "/Contact", link:"Contact"},
+        {path: "/Blogs", link:"Blogs"}
+    ]
     return (
-        <div>
-            <h1>Navbar</h1>
-        </div>
+        <header className='bg-black'>
+            <nav className='px-4 py-4'>
+                <a href="/" className='text-xl font-bold text-white'>TWIT<span className='text-orange-500'>Blog</span></a>
+
+                <ul className='flex gap-12 text-lg'>
+                    {
+                        navItems.map(({path, link})=> <li className='text-white' key={path}>
+                            <NavLink to={path}>{link}</NavLink>
+                        </li>)
+                    }
+                </ul>
+            </nav>
+        </header>
     );
 };
 
