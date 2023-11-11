5c39deef0dae4e18872549720d9d7bd895e74f3a
Rudro Dhar <rudro149187@bsdi-bd.org>
2023-11-11 14:15:22 +0600

    Banner Done

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import './App.css'
+import Banner from './components/Banner';
 const App = () => {
   return (
     <>
       <Navbar/>
       <Outlet/>
+      <Banner/>
     </>
   );
 };
---
+++ src/components/Banner.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import {FaArrowRight} from "react-icons/fa6"
+
+const Banner = () => {
+    return (
+        <div className='px-4 py-32 bg-black mx-auto'>
+            <div className='text-white text-center'>
+                <h1 className='text-5xl lg:text-7xl leading-snug font-bold mb-5'>Welcome to our blog</h1>
+                <p className='text-gray-100 lg:w-3/5 mx-auto mb-5 font-primary'>Start your blog today a comunity of writers and readers who are passionate about sharing there stories and ideas.We offer everythnig you need to to get started from helpfull tips and tutorials.</p>
+
+                <div>
+                    <Link to='/' className='font-medium hover:text-orange-500 inline-flex items-center py-1'>Learn More<FaArrowRight className='mt-1 ml-2'/></Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
